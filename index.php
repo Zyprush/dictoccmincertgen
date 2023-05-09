1de8f4f8c75f065c2d95f0d01e1e4db50f630321
@githubAdmin123 <certgendict@gmail.com>
2023-05-09 10:32:54 +0800

    index fix

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    include('config/authentication.php');
-    include('includes/header.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/config/authentication.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/includes/header.php');
     
     // Redirect to dashboard if user is already logged in
     if(isset($_SESSION['user_id'])) {
@@ -13,5 +13,5 @@
 ?>
 
 <?php
-    include('includes/footer.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php');
 ?>
